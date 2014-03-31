40d6bdcc60292aad51ac678c65fa3a04003b7cb8
Paweł Lipski <pawel.p.lipski@gmail.com>
2014-03-31 13:26:06 +0200

    Add main def and processor classes skels

---
+++ main.py
@@ -0,0 +1,53 @@
+
+
+def compute_corpus_wide_stats(articles, corp_procs):
+
+	for (article, classification) in articles:
+
+		for corp_proc in corp_procs:
+			corp_proc.switch_article(classification)
+
+		for w in article.split(' '):
+			for corp_proc in corp_procs:
+				corp_proc.record_word(w)
+			for c in w:
+				for corp_proc in corp_procs:
+					corp_proc.record_char(c)
+
+
+def pass_stats_to_article_processors(procs):
+	for (corp_proc, art_proc) in procs:
+		stats = corp_proc.get_corpus_wide_stats()
+		art_proc.proc.set_corpus_wide_stats(stats)
+
+
+def compute_feature_vectors_for_articles(articles, art_procs):
+	for (article, _) in articles:
+
+		for w in article.split(' '):
+			for art_proc in art_procs:
+				art_proc.record_word(w)
+			for c in w:
+				for art_proc in art_procs:
+					art_proc.record_char(c)
+
+		for art_proc in art_procs:
+			v = art_proc.get_features()
+			art_proc.clean_up()
+
+
+def main():
+
+	procs = []
+	articles = [ ("Lorem ipsum", 1), ("dolor sit amet", 2) ]
+
+	corp_procs = [corp_proc for (corp_proc, art_proc) in procs]
+	compute_corpus_wide_stats(articles, corp_procs)
+
+	pass_stats_to_article_processors(procs)
+
+	art_procs = [art_proc for (corp_proc, art_proc) in procs]
+	compute_feature_vectors_for_articles(articles, art_procs)
+
+if __name__ == '__main__':
+	main()
---
+++ processors.py
@@ -0,0 +1,42 @@
+
+class Processor:
+	""" Typically, only one of the methods record_char and record_word
+	 will be non-empty, but both will be called on the corresponding events
+	 (on char collected/on word complete)"""
+
+	def record_char(self, c):
+		"""Non-empty for e.g. CNG"""
+		pass
+
+	def record_word(self, w):
+		"""Non-empty for e.g. FW, POS, CW"""
+		pass
+
+
+class CorpusProcessor(Processor):
+
+	def switch_article(self, classification):
+		"""Start collecting data for the new article
+		relevant e.g. when IG is being computed, like for CW and CNG"""
+		pass
+
+	def get_corpus_wide_stats(self):
+		"""The result can be even a simple list, dict or a tuple -
+		 no need to return an instance of a specialized object"""
+		pass
+
+
+class ArticleProcessor(Processor):
+
+	def set_corpus_wide_stats(self, stats):
+		self.stats = stats
+
+	def clean_up(self):
+		"""Used to clean up the stored state,
+		just before the new article is started"""
+		pass
+
+	def get_features(self):
+		"""Return a NEWLY CREATED flat list with features
+		for the moment being - typically clean_up will be called after the call to get_features"""
+		pass

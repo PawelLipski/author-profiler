ada3e845a42109fe60ed861c51ba27f9e19cc3f8
Paweł Lipski <pawel.p.lipski@gmail.com>
2014-04-01 19:16:11 +0200

    Rename variables and fields in CWCorpusProcessor

--- cw.py
+++ cw.py
@@ -4,7 +4,7 @@ from math import log
 
 class CWCorpusProcessor(CorpusProcessor):
 
-	MOST_COMMON_TAKEN = 100
+	MOST_COMMON_TAKEN = 1000
 	HIGHEST_INFOGAINS_TAKEN = 10
 
 
@@ -13,8 +13,8 @@ class CWCorpusProcessor(CorpusProcessor):
 		self.currect_art_category = 0
 		self.current_art_word_set = set()
 
-		self.freq_entries_by_word = {}
-		self.articles_in_category = self.new_freq_entry()
+		self.arts_by_categories_by_word = {}
+		self.arts_by_category = self.new_arts_by_category_array()
 
 
 	def switch_article(self, clsfn):
@@ -23,41 +23,32 @@ class CWCorpusProcessor(CorpusProcessor):
 		self.currect_art_category = clsfn.get_category_number()
 		self.current_art_word_set = set()
 
-		self.articles_in_category[self.currect_art_category] += 1
+		self.arts_by_category[self.currect_art_category] += 1
 
 
-	def first_occurred_in_current_article(self, w):
+	def first_occurred_in_current_art(self, w):
 
 		return w not in self.current_art_word_set
 
-	def new_freq_entry(self):
+
+	def new_arts_by_category_array(self):
 
 		return [0] * Classification.NUMBER_OF_CATEGORIES
 
-	def freq_entry_total(self, freq_entry):
 
-		return sum(freq_entry)
-
-	def freq_entry_female(self, freq_entry):
-
-		return sum(freq_entry[:4])
-
-	def freq_entry_male(self, freq_entry):
-
-		return sum(freq_entry[4:])
 
 	def record_word(self, w):
 
-		if self.first_occurred_in_current_article(w):
+		if self.first_occurred_in_current_art(w):
 
 			self.current_art_word_set.add(w)
 
-			freq_entry = self.freq_entries_by_word.get(w)
+			arts_by_categories_for_the_word = self.arts_by_categories_by_word.get(w)
 
-			if not freq_entry:
-				freq_entry = self.freq_entries_by_word[w] = self.new_freq_entry()
+			if not arts_by_categories_for_the_word:
+				arts_by_categories_for_the_word = self.arts_by_categories_by_word[w] = self.new_arts_by_category_array()
 
-			freq_entry[self.currect_art_category] += 1
+			arts_by_categories_for_the_word[self.currect_art_category] += 1
 
 
 	def get_corpus_wide_stats(self):
@@ -76,10 +67,11 @@ class CWCorpusProcessor(CorpusProcessor):
 	def get_most_common(self):
 
 		# just not to miss the minus
-		get_minus_freq = lambda (word, freq_entry): ((((-1)))) * self.freq_entry_total(freq_entry)
+		get_minus_total_freq = lambda (word, arts_by_categories_for_the_word): \
+			((((-1)))) * sum(arts_by_categories_for_the_word)
 
-		word_freq_list = self.freq_entries_by_word.items()
-		sorted_by_freq_desc = sorted(word_freq_list, key=get_minus_freq)
+		word_entry_list = self.arts_by_categories_by_word.items()
+		sorted_by_freq_desc = sorted(word_entry_list, key=get_minus_total_freq)
 
 		return sorted_by_freq_desc[:self.MOST_COMMON_TAKEN]
 
@@ -88,9 +80,9 @@ class CWCorpusProcessor(CorpusProcessor):
 
 		infogains = []
 
-		for (word, freq_entry) in words_most_common:
-			print word, self.freq_entry_female(freq_entry), self.freq_entry_male(freq_entry)
-			ig = self.get_infogain(freq_entry)
+		for (word, arts_by_categories_for_the_word) in words_most_common:
+			print word, arts_by_categories_for_the_word
+			ig = self.get_infogain(arts_by_categories_for_the_word)
 			infogains.append((word, ig))
 
 		get_minus_ig = lambda (word, ig): -ig
@@ -101,70 +93,38 @@ class CWCorpusProcessor(CorpusProcessor):
 		words_highest_ig = [word for (word, ig) in infogains[:self.HIGHEST_INFOGAINS_TAKEN]]
 		return words_highest_ig
 
-	def get_infogain(self, articles_in_category_containing_the_word):
+
+	def get_infogain(self, arts_by_categories_for_the_word):
 
 		log2_or_zero = lambda x: log(x, 2) if x else 0.0
 		div_or_zero = lambda m, n: (float(m) / n) if n else 0.0
 
-		total = sum(self.articles_in_category)
-		total_containing = sum(articles_in_category_containing_the_word) # sum over categories
+		total = sum(self.arts_by_category)
+		total_containing = sum(arts_by_categories_for_the_word) # sum over categories
 		total_not_containing = total - total_containing
 
 		entropy_word_present = 0.0
-		for i in range(len(articles_in_category_containing_the_word)):
-			in_category_containing = articles_in_category_containing_the_word[i]
+		for i in range(len(arts_by_categories_for_the_word)):
+			in_category_containing = arts_by_categories_for_the_word[i]
 			prop_containing_is_in_category = div_or_zero(in_category_containing, total_containing)
-			print in_category_containing, total_containing, '->', prop_containing_is_in_category
 			entropy_word_present += (((-1))) * prop_containing_is_in_category * log2_or_zero(prop_containing_is_in_category)
+
 		prop_word_present = div_or_zero(total_containing, total)
 
+
 		entropy_word_absent = 0.0
-		for i in range(len(articles_in_category_containing_the_word)):
-			in_category_not_containing = self.articles_in_category[i] - articles_in_category_containing_the_word[i]
+		for i in range(len(arts_by_categories_for_the_word)):
+			in_category_not_containing = self.arts_by_category[i] - arts_by_categories_for_the_word[i]
 			prop_not_containing_is_in_category = div_or_zero(in_category_not_containing, total_not_containing)
 			entropy_word_absent += (((-1))) * prop_not_containing_is_in_category * log2_or_zero(prop_not_containing_is_in_category)
+
 		prop_word_absent = div_or_zero(total_not_containing, total)
+
 
 		print prop_word_present, entropy_word_present, prop_word_absent, entropy_word_absent
 		ig = - prop_word_present * entropy_word_present - prop_word_absent * entropy_word_absent
 		return ig
 
-	def get_infogain_old(self, freq_entry):
-
-		log2_or_zero = lambda x: log(x, 2) if x else 0.0
-		div_or_zero = lambda m, n: (float(m) / n) if n else 0.0
-
-		# computed according to http://www.cise.ufl.edu/~ddd/cap6635/Fall-97/Short-papers/2.htm
-		total_containing = self.freq_entry_total(freq_entry)
-		female_containing = self.freq_entry_female(freq_entry)
-		male_containing = self.freq_entry_male(freq_entry)
-		f = div_or_zero(female_containing, total_containing)
-		m = div_or_zero(male_containing, total_containing)
-		print total_containing, female_containing, male_containing, f, m
-
-		total = self.freq_entry_total(self.articles_in_category)
-		total_female = self.freq_entry_female(self.articles_in_category)
-		total_male = self.freq_entry_male(self.articles_in_category)
-
-		total_not_containing = total - total_containing
-		female_not_containing = total_female - female_containing
-		male_not_containing = total_male - male_containing
-		nf = div_or_zero(female_not_containing, total_not_containing)
-		nm = div_or_zero(male_not_containing, total_not_containing)
-		print total_not_containing, female_not_containing, male_not_containing, nf, nm
-
-		entropy_word_present = - m * log2_or_zero(m) - f * log2_or_zero(f)
-		entropy_word_absent = - nm * log2_or_zero(nm) - nf * log2_or_zero(nf)
-
-		# use div_or_zero in the case corpus is empty (self.total_number() == 0)
-		prop_word_present = div_or_zero(total_containing, total)
-		prop_word_absent = div_or_zero(total_not_containing, total)
-
-		# it's not really the infogain,
-		# since the formula also incorporates the part for entropy H(S) -
-		# this can be safely skipped, however
-		ig = - prop_word_present * entropy_word_present - prop_word_absent * entropy_word_absent
-		return ig
 
 
 class CWArticleProcessor(ArticleProcessor):

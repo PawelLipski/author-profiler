75b1ba9114e4d183ff92338a61b3b99f9f3007f1
Paweł Lipski <pawel.p.lipski@gmail.com>
2014-04-01 09:03:05 +0200

    Extract numerical category from a Classification object

--- processors.py
+++ processors.py
@@ -5,6 +5,12 @@ class Classification:
 		self.gender = gender
 		self.age = age
 
+	CATEGORY_FOR_GENDER = { 'F': 0, 'M': 4 }
+	CATEGORY_FOR_AGE = { 18: 0, 25: 1, 35: 2, 50: 3 }
+
+	def get_category_number(self):
+		return self.CATEGORY_FOR_GENDER[self.gender] + self.CATEGORY_FOR_AGE[self.age]
+
 
 class Processor:
 	""" Typically, only one of the methods record_char and record_word

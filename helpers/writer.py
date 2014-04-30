344bc1ba36a3ac7a1fffd76a6589a3badf431915
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-04-30 13:37:16 +0200

    Fix up indentation in output XMLs

--- helpers/writer.py
+++ helpers/writer.py
@@ -5,13 +5,11 @@ class PredictionWriter:
 	def output_prediction(outdir, authorid, cls):
 
 		gender, age = str(cls).split(', ')
-		tag = """
-		<author id="%s"
-			type="blog|twitter|socialmedia|reviews"
-			lang="en|es"
-			age_group="%s"
-			gender="%s"
-		/>""" % (authorid, age, gender)
+		tag = """<author id="%s"
+		type="blog|twitter|socialmedia|reviews"
+		lang="en|es"
+		age_group="%s"
+		gender="%s"/>\n""" % (authorid, age, gender)
 
 		out = open(outdir + '/' + authorid + '.xml', 'w')
 		out.write(tag)

025c64c9484fb40f5295ab25a5be0dfb2c139e8f
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-01 18:15:59 +0200

    Create quick-run.sh

--- scripts/quick-run-for-3.sh
+++
@@ -1,5 +0,0 @@
-
-./train.py -i ../corpora/pan14-author-profiling-training-3/ -o ../models/
-
-./classify.py -i ../corpora/pan14-author-profiling-training-3/ -m ../models/ -o ../classify-outputs/
-
---
+++ scripts/quick-run.sh
@@ -0,0 +1,5 @@
+
+./train.py -i ../corpora/pan14-author-profiling-training-$1/ -o ../models/
+
+./classify.py -i ../corpora/pan14-author-profiling-training-$1/ -m ../models/ -o ../classify-outputs/
+

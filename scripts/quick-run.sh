6db6c3d445dbb2e2ce5d0ad9634b52b9c7044aec
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-05 18:43:22 +0200

    Accept --disjoint option in train.py

--- scripts/quick-run.sh
+++ scripts/quick-run.sh
@@ -1,5 +1,5 @@
 
-./train.py -i ../corpora/pan14-author-profiling-training-$1/ -o ../models/
+./train.py $2 -i ../corpora/pan14-author-profiling-training-$1/ -o ../models/
 
 ./classify.py -i ../corpora/pan14-author-profiling-training-$1/ -m ../models/ -o ../classify-outputs/
 
--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Creates model for author profiling using given corpus.')
 parser.add_argument('-i', metavar='corpus_dir', required=True, help='path to dir containing *.xml files')
 parser.add_argument('-o', metavar='model_dir', required=True, help='path to empty dir where model will be saved')
+parser.add_argument('--disjoint', action='store_true', help='if present, forces use of disjoint (gender/age) classifier;' +\
+	' otherwise, joint classifier is used')
 args = parser.parse_args()
 
 from helpers import Configuration
@@ -17,7 +19,7 @@ import pickle
 import sys
 import os.path
 
-classifier = JointClassifier()
+classifier = DisjointClassifier() if args.disjoint else JointClassifier()
 
 if os.path.isfile(Configuration.CorpusDirectory + '/truth.txt'):
 	reader = TrainDataReader(Configuration.CorpusDirectory + '/*.xml', Configuration.CorpusDirectory + '/truth.txt')

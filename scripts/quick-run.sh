f3f26c6e198e311e0700307f67d9b60e950b30f4
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-05 18:48:05 +0200

    Fix up directories in quick-run.sh

--- scripts/quick-run.sh
+++ scripts/quick-run.sh
@@ -1,5 +1,5 @@
 
 ./train.py $2 -i ../corpora/pan14-author-profiling-training-$1/ -o ../models/
 
-./classify.py -i ../corpora/pan14-author-profiling-training-$1/ -m ../models/ -o ../classify-outputs/
+./classify.py -i ../corpora/pan14-author-profiling-test-$1/ -m ../models/ -o ../classify-outputs/
 

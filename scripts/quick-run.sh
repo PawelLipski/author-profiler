b4f8f91b9ede9cae680de86a50f1e2edf6bde173
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-07 08:42:27 +0200

    Fix up launch script

--- scripts/quick-run.sh
+++ scripts/quick-run.sh
@@ -2,7 +2,5 @@
 N=$1
 shift
 
-./train.py $@ -i ../corpora/pan14-author-profiling-training-$N/ -o ../models/
-
-./classify.py -i ../corpora/pan14-author-profiling-test-$N/ -m ../models/ -o ../classify-outputs/
+./train.py $@ -i ../corpora/pan14-author-profiling-training-$N/ -o ../models/ && ./classify.py -i ../corpora/pan14-author-profiling-test-$N/ -m ../models/ -o ../classify-outputs/
 
